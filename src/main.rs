005d9bd0307adf14b87307576c511afae9ab6e04
user-debian <74452307+MaSTerAK-902@users.noreply.github.com>
2023-08-24 13:51:54 +0900

    crate の追加

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,26 @@
 /// # Rust Todo アプリケーションのサンプル作成
 /// ### ドキュメントの作成方法について
 /// "///" 使ってコメントアウトすることで、ドキュメントを作成することができる。
+/// ### crateの追加・削除
+/// '''
+/// cargo add ~~ で追加
+/// cargo rm ~~ で削除
+/// '''
+/// Todo で使用するcrateの一覧
+/// ・rusqlite SQLiteを使うためのクレート
 
+/// ## Todoアプリケーションのデータ構造
+/// ### Taskcheck
+/// bool型で、このTaskの完了未完了を確認します。
+/// ### task_num
+/// このタスクの検索用名です。重複は許されません一意の識別子です。
+/// ### task_event
+/// このタスクの、要件内容です。
 #[allow(dead_code)] 
 struct ToDo {
     task_check: bool,
-    name: String,
-    event: String,
+    task_num: u32,
+    task_event: String,
 }
 
 fn main() {
@@ -19,8 +33,8 @@ fn main() {
         // String構造体もスタックに入りますが、
         // ヒープに入るデータの参照アドレスが一つ入ります。
         task_check: false,
-        name: String::from("タイトル"),
-        event: String::from("詳細を記入する。"),
+        task_num: 1,
+        task_event: String::from("詳細を記入する。"),
 
         //構造体からデータを取り出す際は、構造体のフィールドは演算子"."で取り出すことができます。
         // "今日のイベント名は、{}で詳細は{}です。", event_info.name, event_info.event 
